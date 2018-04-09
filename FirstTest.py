e902f244620d73c68bb4cc1a843c2e36db1a0ea0
anuyad <38095384+anuyad@users.noreply.github.com>
2018-04-09 16:05:23 +0530

    Update FirstTest.py

--- FirstTest.py
+++ FirstTest.py
@@ -1,4 +1,4 @@
-def print_something(name="something", age="at present unknown"):
+def print_something(name="something", age="unknown"):
     print("My name is", name, "and my age is",age)
 
 print_something(24)
